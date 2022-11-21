588000e0daca72c94b36f3cdad5ec61d6f3c455d
Angel Reyna <74248282+hrxrx@users.noreply.github.com>
2022-11-21 11:27:13 +0200

    Update script.js

--- script.js
+++ script.js
@@ -340,6 +340,174 @@ savaites_dienos.push("penktadienis"); //
 console.log('pakeistas masyvas ${savaites_dienos}');
 
 
+
+// let courses = [
+//     {
+//         pavadinimas: "JS",
+//         aprasymas: "labai lengva programavimo kalba",
+//         lektorius: "Jolita",
+//         kaina: {
+//             akcijai: 50,
+//             pagrindine: 100,
+//             galutine: 30
+//         },
+//         tag: ["js", "programing", "web"]
+//     },
+//     {
+//         pavadinimas: "SQL",
+//         aprasymas: "strukturizuota database kalba",
+//         lektorius: "Dovydas",
+//         kaina: {
+//             akcijai: 20,
+//             pagrindine: 80,
+//             galutine: 10
+//         },
+//         tag: ["sql", "database", "it"]
+//     },
+//     {
+//         pavadinimas: "HTML",
+//         aprasymas: "ne programavimo kalba",
+//         lektorius: "Zigmas",
+//         kaina: {
+//             akcijai: 19.99,
+//             pagrindine: 89.99,
+//             galutine: 9.99
+//         },
+//         tag: ["html", "markup", "webpages"]
+//     },
+// ]
+
+// for (i in courses) {
+  
+// }
+
+
+// for (let pavadinimas in courses) {
+//     //console.log(courses);
+//     for(html in pavadinimas){
+//         //console.log(courses)
+
+//     }
+// }
+
+
+
+
+// for (i in courses) {
+//     //console.log(courses[i])
+// }
+//     for (j in courses[i]) {
+//         console.log(courses[i][j])};
+//         console.log('*****************************************')
+//         if (!Array.isArray(courses[i][j]) && typeof courses[i][j] === "object") {
+//             console.log(`Vardinu knygas ${j}s`);
+            
+//             //console.log(courses[i][j])
+
+
+            
+//            for(a in [i][j]) {
+//             console.log(`${a} ${courses[i][j][a]}`);
+//             }
+//         } else if (Array.isArray(courses[i][j])) {
+//             for (a in courses[i][j]) {
+//                 console.log(`${a} ${courses[i][j][a]}`);
+//             }
+
+
+//         }
+//         410
+        
+        
+//        /////// ){
+//       ///////////  } else if (typeOf courses[i][j] === "object") {
+//       //////      console.log(courses[i][j])
+//        /////// }
+//     //////}
+// //////}
+
+
+
+// let duomenys ={
+//     "id": "0001",
+//     "type": "donut",
+//     "name": "Cake",
+//     "ppu": 0.55,								
+//     "topping":
+//         [
+//             { "id": "5001", "type": "None" },
+//             { "id": "5002", "type": "Glazed" },
+//             { "id": "5005", "type": "Sugar" },
+//             { "id": "5007", "type": "Powdered Sugar" },
+//             { "id": "5006", "type": "Chocolate with Sprinkles" },
+//             { "id": "5003", "type": "Chocolate" },
+//             { "id": "5004", "type": "Maple" }
+//         ]
+// }
+
+
+
+// for (let i in duomenys){
+//         //console.log(i);
+//         //console.log(duomenys[i]);
+//     if(!Array.isArray(duomenys[i])){
+//         console.log(` ${i}: ${duomenys[i]}`);
+//         } else {
+//     for (let j in duomenys[i]){
+//         console.log(` ${i}: `); 
+//     for (let k in duomenys[i][j]){
+//         console.log(` ${k} ${duomenys[i][j][k]}`);
+//         //console.log(k); 
+//                 }
+//             }
+//         }
+//     }      
+
+
+const data = {
+        0: {section: 'theater', subsection: '', title: 'Review: ‘Morning’s at Seven’ Awakens Again, Only to Hit Snooze', abstract: 'Paul Osborn’s 1930s play is revived, with its thin… somewhat bitter slice of small town life intact.', url: 'https://www.nytimes.com/2021/11/15/theater/mornings-at-seven-review.html'},
+        1: {section: 'arts', subsection: '', title: 'The Art of Botox', abstract: 'How facial muscle paralysis insinuated itself into our emotional and creative lives.', url: 'https://www.nytimes.com/2021/11/15/arts/botox.html'},
+        2: {section: 'arts', subsection: 'music', title: 'Taylor Swift’s ‘All Too Well’ and the Weaponization of Memory', abstract: 'The new 10-minute version of a bitter breakup song…power imbalance in the relationship it describes.', url: 'https://www.nytimes.com/2021/11/15/arts/music/taylor-swift-all-too-well.html'},
+        3: {section: 'arts', subsection: 'design', title: 'Blue-Chip Art From Bitter Macklowe Divorce Brings $676 Million at Sotheby’s', abstract: 'A Sotheby’s executive called the court-ordered sal… most valuable single-owner auction ever staged.”', url: 'https://www.nytimes.com/2021/11/15/arts/design/sothebys-macklowe-auction-rothko-warhol.html'},
+        4: {section: 'arts', subsection: 'music', title: 'If Remote Work Empties Downtowns, Can Theaters Fill Their Seats?', abstract: 'Since the pandemic, San Francisco has embraced wor…g if weeknight audiences are a thing of the past.', url: 'https://www.nytimes.com/2021/11/15/arts/music/san-francisco-theaters-concert-halls.html'},
+        5: {section: 'arts', subsection: 'dance', title: 'Twyla Tharp: ‘Each One of the Dances Is My Hope for a Perfect World’', abstract: 'In “Twyla Now,” a program of works featuring balle…y Center, Tharp merges the past with the present.', url: 'https://www.nytimes.com/2021/11/15/arts/dance/twyla-tharp-twyla-now.html'},
+        6: {section: 'arts', subsection: 'television', title: '‘The Sex Lives of College Girls’ Pursues an Awkward Education', abstract: 'The young stars of this new HBO Max comedy, cocrea…fication and messiness of undergraduate intimacy.', url: 'https://www.nytimes.com/2021/11/15/arts/television/the-sex-lives-of-college-girls.html'},
+        7: {section: 'arts', subsection: 'music', title: 'Filings Show Continuing Battle Over Britney Spears’s Finances', abstract: 'New court documents reveal a heated exchange betwe…rs and the former business manager of her estate.', url: 'https://www.nytimes.com/2021/11/15/arts/music/britney-spears-conservatorship-filings.html'},
+        8: {section: 'arts', subsection: 'television', title: 'First Asian American Muppet Arrives on ‘Sesame Street’', abstract: 'Ji-Young, a guitar-playing Korean American charact…ing children’s show starting on Thanksgiving Day.', url: 'https://www.nytimes.com/2021/11/15/arts/television/new-sesame-street-character-ji-young.html'},
+        9: {section: 'theater', subsection: '', title: 'Jeanna de Waal Has Already Forgotten About That ‘Diana’ Film', abstract: 'The Netflix version of Broadway’s “Diana, the Musi…tress is staying positive ahead of opening night.', url: 'https://www.nytimes.com/2021/11/16/theater/jeanna-de-waal-diana-broadway.html'},
+        10: {section: 'arts', subsection: 'music', title: 'Latin Grammys 2021: Complete Nominees List', abstract: 'Here are the nominees for the 22nd annual ceremony.', url: 'https://www.nytimes.com/2021/11/16/arts/music/latin-grammys-nominees-list.html'},
+        11: {section: 'books', subsection: 'review', title: 'Neal Stephenson’s Mad, Mad, Mad, Mad World', abstract: '“Termination Shock,” his latest novel, is set in a…one haywire from the impacts of climate disaster.', url: 'https://www.nytimes.com/2021/11/16/books/review/neal-stephenson-termination-shock.html'},
+        12: {section: 'books', subsection: 'review', title: 'The True-Life Inspiration for Dostoyevsky’s Famous Murderer', abstract: 'The model for Raskolnikov, the tortured killer in …trait of Dostoyevsky and the making of the novel.', url: 'https://www.nytimes.com/2021/11/16/books/review/the-sinner-and-the-saint-kevin-birmingham.html'},
+        13: {section: 'books', subsection: 'review', title: 'The Cutting Mind and Romantic Heart of Elizabeth Hardwick', abstract: '“A Splendid Intelligence,” by Cathy Curtis, is the…iography of the much admired critic and novelist.', url: 'https://www.nytimes.com/2021/11/16/books/review/ca…-intelligence-the-life-of-elizabeth-hardwick.html'},
+        14: {section: 'books', subsection: 'review', title: 'Seven Immigrant Women Who Changed the Way Americans Eat', abstract: '“Taste Makers,” by Mayukh Sen, features women who,…Americans to the dishes of their native cultures.', url: 'https://www.nytimes.com/2021/11/16/books/review/taste-makers-mayukh-sen.html'},
+        15: {section: 'books', subsection: 'review', title: 'Bildungsromans From Around the World', abstract: 'New fiction by Elif Shafak, S.J. Sindu and Bisi Adjapon.', url: 'https://www.nytimes.com/2021/11/16/books/review/bl…apon-the-island-of-missing-trees-elif-shafak.html'},
+        16: {section: 'books', subsection: 'review', title: 'How the Greek Revolution of 1821 Led to the Global System of Nation-States', abstract: 'Mark Mazower’s “The Greek Revolution” examines a c…ry-old event that continues to reverberate today.', url: 'https://www.nytimes.com/2021/11/16/books/review/the-greek-revolution-mark-mazower.html'},
+        17: {section: 'books', subsection: 'review', title: 'A Debut Novel Where Small Moments Add Up to Something Big', abstract: 'In “A Little Hope,” Ethan Joella explores quiet lives in small-town Connecticut.', url: 'https://www.nytimes.com/2021/11/16/books/review/a-little-hope-ethan-joella.html'},
+        18: {section: 'books', subsection: '', title: 'In Another Trump Book, a Journalist’s Belated Awareness Steals the Show', abstract: '“Betrayal,” by Jonathan Karl, is a record of Trump…e House correspondent’s attempt to understand it.', url: 'https://www.nytimes.com/2021/11/16/books/review-betrayal-final-act-trump-show-jonathan-karl.html'},
+        19: {section: 'arts', subsection: 'television', title: 'Seth Meyers: Steve Bannon Fancies Himself to Be Logan Roy', abstract: 'Meyers said Bannon was more like “a coked-up flunk…red some documents and accidentally screw it up.”', url: 'https://www.nytimes.com/2021/11/16/arts/television…steve-bannon-fancies-himself-to-be-logan-roy.html'},
+        20: {section: 'arts', subsection: 'music', title: 'Terence Wilson, Key Part of Reggae Band UB40, Dies at 64', abstract: 'As Astro with a popular racially diverse British g… he added rap vocals to hits like “Red Red Wine.”', url: 'https://www.nytimes.com/2021/11/15/arts/music/terence-wilson-astro-dead.html'},
+        21: {section: 'books', subsection: '', title: 'Let’s Tackle Your Book Shopping List', abstract: 'There’s no doubt that books can make for the most …he perfect one can be nerve-racking. Let us help.', url: 'https://www.nytimes.com/interactive/2021/11/15/books/books-to-give.html'},
+        22: {section: 'books', subsection: '', title: '‘Medusa’s Ankles,’ a Selection of A.S. Byatt’s Wildly Imagined Stories Across Three Decades', abstract: 'Byatt has been an articulate advocate for tapping …ories tend to stay in realms of thinner altitude.', url: 'https://www.nytimes.com/2021/11/15/books/review-medusas-ankles-a-s-byatt.html'}
+        }
+
+
+
+
+console.log(data);
+
+for (let i in data){
+    //console.log(i);
+    for (let j in data[i]){
+        //console.log(j);
+        for (let k in data[i][j]){
+        //console.log(k);
+        for (let l in data [i][j][k]){
+            console.log(l);
+        }
+        }
+    }
+}
+
 //filter method
 let atrinkta_diena = savaites_dienos.filter( diena => diena == "penktadienis");
 console.log(atrinkta_diena);
